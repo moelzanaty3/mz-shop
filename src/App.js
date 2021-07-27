cdd62598b79474e534627bad5e206c0621e9b94d
Abdalla Mush0 <abdalla.magdy@gmail.com>
2021-07-28 01:12:18 +0200

    Added the required routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { deleteProduct, getAllProduct } from './api/ProductAPI'
 import ProductList from './containers/ProductList'
+import { Switch, Route } from 'react-router-dom'
+import ProductPage from './components/ProductPage'
 
 class App extends Component {
   state = {
@@ -25,12 +27,22 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <ProductList
-          products={this.state.products}
-          onDeleteProduct={this.handleDeleteProduct.bind(this)}
-        />
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <div>
+            <ProductList
+              products={this.state.products}
+              onDeleteProduct={this.handleDeleteProduct.bind(this)}
+            />
+          </div>
+        </Route>
+        <Route path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route path="/">
+          <img src="https://i.stack.imgur.com/6M513.png" alt="Error:404 Page not found "></img>
+        </Route>
+      </Switch>
     )
   }
 }
